6931aeed00ead7d7ed7abe995fc8f1c0c20d00ec
root <root@ip-172-31-1-89.ap-south-1.compute.internal>
2022-05-30 05:53:28 +0000

    new helloworld progarm

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,5 @@
-// Your First Program
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
 }
